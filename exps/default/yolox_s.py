e7ede40425a5619869cba1847bbebb537f3a280f
kevin wu <wzflearning@163.com>
2023-11-25 17:18:42 +0800

    first commit

---
+++ exps/default/yolox_s.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.num_classes = 6
+        # self.depth = 0.33
+        # self.width = 0.50
+
+        self.depth = 1.00
+        self.width = 1.00
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
